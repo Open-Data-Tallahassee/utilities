569d24ef1a7fccc19ca91585373a11baac1fa762
shelby <53214352+shelbygreen@users.noreply.github.com>
2021-12-16 18:14:21 -0500

    updated map for disconnections by zip (#37)

--- map/src/components/Map/Map.js
+++ map/src/components/Map/Map.js
@@ -1,38 +1,40 @@
 // import modules
 import React, { useEffect, useRef } from 'react'
-import mapboxgl from 'mapbox-gl'
+import mapboxgl from '!mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 import { fromJS } from 'immutable'
 import { siteMetadata } from '../../../gatsby-config'
 import { sources, layers } from '../../../config/map'
 import styled from '../../../util/style'
+import './map.css'
 
 // wrapper component
-const Wrapper = styled.div`
-    height: 100%;
-`
+// const Wrapper = styled.div`
+//     height: 100%;
+// `
 
-const MapContainer = styled.div`
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0; 
-`
-const Sidebar = styled.div`
-    position: absolute;
-    overflow-y: auto;
-    height: 200px;
-    top: calc(47px + 30px);
-    z-index: 4000;
-    background-color: #fff;
-    width: 340px;
-    padding: 10px;
-    border-radius: 0;
-    color: #29323c;
-    right: 30px;
-    margin: auto;
-    box-shadow: 0 0 0 1px rgba(16, 22, 26, 0.1), 0 1px 1px rgba(16, 22, 26, 0.2), 0 2px 6px rgba(16, 22, 26, 0.2);
-`
+// const MapContainer = styled.div`
+//     position: absolute;
+//     top: 500;
+//     bottom: 0;
+//     left: 0;
+//     right: 0; 
+// `
+// const Sidebar = styled.div`
+//     position: absolute;
+//     overflow-y: auto;
+//     height: 200px;
+//     top: calc(47px + 30px);
+//     z-index: 4000;
+//     background-color: #fff;
+//     width: 340px;
+//     padding: 10px;
+//     border-radius: 0;
+//     color: #29323c;
+//     right: 30px;
+//     margin: auto;
+//     box-shadow: 0 0 0 1px rgba(16, 22, 26, 0.1), 0 1px 1px rgba(16, 22, 26, 0.2), 0 2px 6px rgba(16, 22, 26, 0.2);
+// `
 
 
 // mapbox token
@@ -58,7 +60,7 @@ const Map = () => {
             container: mapContainer.current,
             style: `mapbox://styles/mapbox/light-v10`,
             center: [-84.28, 30.44],
-            zoom: 11, 
+            zoom: 10, 
             minZoom: 5
         })
 
@@ -89,11 +91,11 @@ const Map = () => {
             // when you click a point on the map, query the features under the point and store
             // in the variable 'features'
             const features = map.queryRenderedFeatures(mapElement.point, {
-                layers: ['points-fill']
+                layers: ['polygons-fill']
             })
 
             const html = `
-            <b>Disconnection Date:</b> ${features[0].properties.Date}
+            <b>Number of Disconnections:</b> ${features[0].properties.disconnections}
             `;
 
             // create tooltip variable for the floating card div
@@ -109,7 +111,7 @@ const Map = () => {
             // when you hover over a point on the map, query the features under the point and store
             // in the variable 'features'
             const features = map.queryRenderedFeatures(mapElement.point, {
-                layers: ['points-fill']
+                layers: ['polygons-fill']
             })
             // if there's something under the point (the features variable is not null)
             // then change the style of the cursor to pointer
@@ -124,20 +126,63 @@ const Map = () => {
             map.getCanvas().style.cursor = '';
         });
 
+        // Add geolocate control to the map.
+        map.addControl(
+            new mapboxgl.GeolocateControl({
+                positionOptions: {
+                enableHighAccuracy: true
+            },
+            // When active the map will receive updates to the device's location as it changes.
+            trackUserLocation: true,
+            // Draw an arrow next to the location dot to indicate which direction the device is heading.
+            showUserHeading: true
+            })
+        );
+
+        // add navigation
+        map.addControl(new mapboxgl.NavigationControl());
+
         // clean up on unmount
         return () => map.remove();
     
     }, [])
 
     return (
-    <Wrapper>
-        <MapContainer ref={mapContainer} style={{ width: '100%', height: '100%' }}/>
-        <Sidebar>
-            <div id="floating-card">
-                <b>Over 4,200 City of Tallahassee Utility customers were disconnected from 04/24/2021 - 05/24/2021 for non-payment.</b>
+        <div className="wrapper">
+            {/* title and map */}
+            <div className="main">
+                {/* title */}
+                <div className="title">
+                    <div className='title-text'>Leon County Zip Codes with Utility Shutoffs (9/24/21 to 11/24/2021)</div>
+                    <div id="floating-card" className='floating-card'>Click around the map to see disconnections per zip code</div>
+                </div>
+                {/* map */}
+                <div className="mapContainer">
+                    <div className="map" ref={mapContainer} />
+                </div>
+                {/* legend */}
+                <div className='legend'>
+                    <div className='legend-scale'>
+                        <div className='legend-labels'>
+                            <div><span style={{background:'#fee5d9'}}></span><b>0</b></div>
+                            <div><span style={{background:'#fcbba1'}}></span><b>1-99</b> <br/> 32317</div>
+                            <div><span style={{background:'#fc9272'}}></span><b>100-499</b> <br/> 32311, 32309, 32312</div>
+                            <div><span style={{background:'#fb6a4a'}}></span><b>500-999</b> <br/> 32305, 32310, 32308</div>
+                            <div><span style={{background:'#de2d26'}}></span><b>1,000-1,999</b> <br/> 32303, 32301</div>
+                            <div><span style={{background:'#a50f15'}}></span><b>2000+</b> <br/> 32304</div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            </Sidebar>
-    </Wrapper>
+        </div>
+    // <Wrapper>
+    //     <MapContainer ref={mapContainer} style={{ width: '100%', height: '100%' }}/>
+    //     <Sidebar>
+    //         <div id="floating-card">
+    //             <b>Almost _____ City of Tallahassee Utility customers have been disconnected for non-payment since the utility moratorium ended.</b>
+    //         </div>
+    //     </Sidebar>
+    // </Wrapper>
     )
 
 }
