006528b2385dcf738e723ba1c78d8688dec8b176
shelby <53214352+shelbygreen@users.noreply.github.com>
2021-12-17 00:53:59 -0500

    fixing git issues (#39)

--- map/public/render-page.js
+++ map/public/render-page.js
@@ -13,8 +13,7 @@ function _interopRequireDefault(obj) {
   };
 }
 
-module.exports = _interopRequireDefault;
-module.exports["default"] = module.exports, module.exports.__esModule = true;
+module.exports = _interopRequireDefault, module.exports.__esModule = true, module.exports["default"] = module.exports;
 
 /***/ }),
 
